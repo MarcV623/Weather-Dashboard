c54b26fd7f70af15f459b1a2d3b21d5676ec04d5
marc vargas <marcvargas@marcs-Air.lan>
2023-07-13 16:40:24 -0500

    added starter code

---
+++ script.js
@@ -0,0 +1,8 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-Items');
+const timezoneEl = document.getElementById('timezone');
+const countryEl = document.getElementById('country');
+const weatherforecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
